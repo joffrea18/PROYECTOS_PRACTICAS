f791bfcc0fdd2441b71918ba334e260aee5c92b1
JoffreArias <123706095+joffrea18@users.noreply.github.com>
2024-10-22 14:39:56 +0200

    Actualizar UNDEFINED en Buttonppal.jsx

--- src/Components/ButtonPpal/Buttonppal.jsx
+++ src/Components/ButtonPpal/Buttonppal.jsx
@@ -9,13 +9,18 @@ import Options from './Options';
 const Buttonppal = ({ emailAddress }) => {
 
     const [menu, setMenu] = useState("options");
+    // He metido una mueva variable que me retorne
+    // un valor NO UNDEFINED, pasando este nuevo valor
+    // como props en el Component.
+    // VALIDAR FUNCIONALIDAD
+    const email = emailAddress.toString();
     
     return (
         <div>
              { <>
                 <form action="" className='menu-button'>
                     <Link
-                    to={<Options emailAddress={emailAddress}/>}>
+                    to={<Options email={email}/>}>
                 <Button variant='contained' >
                     <p>MICROSYSCOM</p>
                 </Button>
